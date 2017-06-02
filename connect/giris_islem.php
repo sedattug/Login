c4edaaf401bd6d444e2dbcfeba358f11e4a61c36
Sedat <sedattug@outlook.com>
2017-06-02 16:49:45 +0300

    Add files via upload

---
+++ connect/giris_islem.php
@@ -0,0 +1,33 @@
+﻿<?php
+ session_start();
+ require_once 'db.php';
+
+ if(isset($_POST['btn-login']))
+ {
+  $username = trim($_POST['username']);
+  $user_password = trim($_POST['password']);
+  
+  try
+  { 
+   $stmt = $db->prepare("SELECT * FROM iller WHERE YERADI =:username");
+   $stmt->execute(array(":username"=>$username));
+   $row = $stmt->fetch(PDO::FETCH_ASSOC);
+   $count = $stmt->rowCount();
+   if($count < 1){
+		echo "Kullanıcı kayıtlı değil.";
+   }else{
+	   if($row['PAFTA25']==$user_password){
+		echo "ok"; // log in
+		//$_SESSION['user_session'] = $row['user_id'];
+		$_SESSION['user_session'] = "ORDU";
+	   }else{
+		    echo "Hatalı şifre."; // hata
+	   }
+   } 
+  }
+  catch(PDOException $e){
+   echo $e->getMessage();
+  }
+ }
+
+?>
